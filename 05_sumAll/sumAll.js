dab1cb242245ec996354069e22559abb06ef524b
i1ravenov <i1ravenov@gmail.com>
2023-06-06 09:48:04 +0300

    Solve 5th problem

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(left, right) {
+    if (!Number.isInteger(left) || !Number.isInteger(right) 
+        || left < 0 || right < 0) {
+            return "ERROR";
+        }
+    if (left > right) {
+        const temp = left;
+        left = right;
+        right = temp;
+    }
+    let res = 0;
+    for (let i = left; i <= right; i++) {
+        res += i;
+    }
+    return res;
 };
 
 // Do not edit below this line
