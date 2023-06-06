2a5f04e49a47f101afc4d1a2e85bd874195624bc
i1ravenov <i1ravenov@gmail.com>
2023-06-06 09:57:33 +0300

    Solve the last problem from the lesson set

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  return round((fahrenheit - 32) * 5 / 9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  return round(celsius * 9 / 5 + 32);
 };
+
+function round(num) {
+  return Math.round(num * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
