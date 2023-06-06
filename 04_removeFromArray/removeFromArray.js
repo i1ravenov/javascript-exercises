42b08748cad9e8987850a6fa95627bd08edad6e4
i1ravenov <i1ravenov@gmail.com>
2023-06-06 09:21:46 +0300

    Solve third and fourth problems

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...entriesToRemove) {
+    
+    for (entry of entriesToRemove) {
+        if (arr.includes(entry)) {
+            arr = removeSingle(arr, entry);
+        }
+    }
+    return arr;
 };
+
+function removeSingle(arr, entry) {
+    const res = [];
+    for (e of arr) {
+        if (e === entry) {
+            continue;
+        } else {
+            res.push(e);
+        }
+    }
+    return res;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
