484d245907e5b16980ece92bebfa06ceb0aedf12
i1ravenov <i1ravenov@gmail.com>
2023-06-15 21:58:42 +0300

    Add solutions to calculator problems

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((a, b) => {
+    return a + b;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((a, b) => {
+    return a * b;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (a, pow) {
+  return a ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let res = 1; 
+  for (let i = 2; i <= n; i++) {
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
