4e04799b210ee5d33d1ec0267337544be078dff7
i1ravenov <i1ravenov@gmail.com>
2023-06-15 22:24:55 +0300

    Add solution to palindromes problem

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    let left = 0;
+    let right = str.length - 1;
+    while (left <= right) {
+        while (punctuation.includes(str[left])) left++;
+        while (punctuation.includes(str[right])) right--;
+        if (str[left++] != str[right--]) {
+            return false;
+        }
+    }
+    return true;
 };
+
+const punctuation = "!:;,.? ";
 
 // Do not edit below this line
 module.exports = palindromes;
