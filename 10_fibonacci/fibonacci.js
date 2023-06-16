d2df975b0636a072e33618511f5eaa0b23a3d184
i1ravenov <i1ravenov@gmail.com>
2023-06-16 11:39:42 +0300

    Add solution to fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = +n;
+    if (n <= 0) return "OOPS";
+    if (n <= 2) return 1;
+    let a1 = 1;
+    let a2 = 1;
+    for (let i = 2; i < n; i++) {
+        let temp = a2;
+        a2 = a1 + a2;
+        a1 = temp;
+    }
+    return a2;
 };
 
 // Do not edit below this line
